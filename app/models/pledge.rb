0f48b5e2611f04513715f96cc63e8e33a9733f1f
tylteng <teng.ty4@gmail.com>
2017-12-11 15:48:58 -0500

    created validations for projects.. (start_date, end_date, goal)

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,7 +2,7 @@ class Pledge < ApplicationRecord
   belongs_to :user
   belongs_to :project
 
-  validates :dollar_amount, presence: true
+  validates :dollar_amount, numericality: {greater_than_or_equal_to: 0}, presence: true
   validates :user, presence: true
   validates :project, presence: true
   validate  :owner_cannot_back_own_project
