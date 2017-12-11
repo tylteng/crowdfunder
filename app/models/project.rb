a83140fe82ab17d5d5926384b2df041bdbc47db2
tylteng <teng.ty4@gmail.com>
2017-12-11 17:03:53 -0500

    added checkboxes for categories

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   has_many :rewards
   has_many :pledges
   has_many :users, through: :pledges # backers
+  has_and_belongs_to_many :categories
   belongs_to :user # project owner
 
   validates :title, :description, :goal, :start_date, :end_date, :user_id, presence: true
